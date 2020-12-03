4d6fd378f641d4298522aec15fad8538fdcdb297
mdheerajprdxn <dheerajm@prdxn.com>
2020-12-03 13:01:40 +0530

    [js] syntax fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,10 +37,14 @@ $(document).ready(function () {
   let active = 0;
   var faqHeadings = $(".faq-heading li");
   var faqs = $(".faq-content>li");
+
   showFaq();
+
   faqHeadings.each((index, item) => {
     item.addEventListener("click", handleFaq);
   });
+
+  //   sets the active index no to the one that is clicked
   function handleFaq(e) {
     let clickedHeading = e.target;
     let foundIndex = null;
@@ -52,6 +56,8 @@ $(document).ready(function () {
     active = foundIndex;
     showFaq();
   }
+
+  //   hides all FAQs then shows only one with active index
   function showFaq() {
     faqs.each((index, item) => {
       item.style.display = "none";
