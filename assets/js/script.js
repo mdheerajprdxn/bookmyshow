ae399b0fbab1a8a627bf58f59b825a449a89ccc9
mdheerajprdxn <dheerajm@prdxn.com>
2020-12-03 10:48:17 +0530

    [js] slider done

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,25 +2,34 @@
 
 */
 
+$(document).ready(function () {
+  var owl = $(".campaigns .owl-carousel");
+  owl.owlCarousel({
+    margin: 10,
+    nav: true,
+    loop: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+    },
+  });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  var owl2 = $(".testimonials .owl-carousel");
+  owl2.owlCarousel({
+    margin: 10,
+    nav: true,
+    loop: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+    },
+  });
+});
